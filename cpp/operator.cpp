7c7b2497248c852a1764655c764ff57dbb6316b9
Andrew Ahnn <andrewahnn@gmail.com>
2019-03-08 14:38:28 -0500

    Add cpp

---
+++ cpp/operator.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        if (a > b) {
+            cout << ">\n";
+        }
+        else if (b > a) {
+            cout << "<\n";
+        }
+        else {
+            cout << "=\n";
+        }
+    }
+}
