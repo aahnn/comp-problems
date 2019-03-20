8802d3ee57f986f17cd4a5927173d33475b4a5ac
Andrew Ahnn <andrewahnn@gmail.com>
2019-03-20 18:10:48 -0400

    Fix interesting_drink

--- python/interesting_drink.py
+++ python/interesting_drink.py
@@ -1,23 +1,8 @@
-from time import time
-
-# from bisect import bisect
-# t1 = time()
-# n = int(input())
-# prices = sorted(map(int, input().split()))
-# q = int(input())
-# coins = [int(input()) for _ in range(q)]
-# for coin in coins:
-#     print(bisect(prices, coin))
-# t2 = time()
-# print(t2 - t1)
-
 from bisect import bisect
-t1 = time()
+
 n = int(input())
 prices = sorted(map(int, input().split()))
 q = int(input())
-for i in range(q):
-    m = int(input())
-    print(bisect(prices, m))
-t2 = time()
-print(t2 - t1)
+coins = [int(input()) for _ in range(q)]
+for coin in coins:
+    print(bisect(prices, coin))
