89942f7c463e5c442559ecb2fb45cf702551efc3
Andrew Ahnn <andrewahnn@gmail.com>
2019-05-15 22:30:51 -0700

    Add polycarp_training

---
+++ cpp/polycarp_training.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> contests(n);
+    for (int i = 0; i < n; i++) {
+        cin >> contests[i];
+    }
+    sort(contests.begin(), contests.end());
+    int k = 1;
+    for (int i = 0; i < n; i++) {
+        if (contests[i] >= k) {
+            k++;
+        }
+    }
+    cout << k - 1;
+}
